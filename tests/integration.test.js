799ab8e96e78506dee62a1647519496700786883
William Durand <will+git@drnd.me>
2021-04-22 15:16:24 +0200

    refactor integration test

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -7,75 +7,41 @@ const { Context } = require('selenium-webdriver/firefox');
 // it by requiring it.
 require('geckodriver');
 
+const { mozCompare } = require('../src');
 const { ALL_VERSIONS } = require('./helpers');
 
 describe(__filename, () => {
   jest.setTimeout(20000);
 
-  it('should match the Firefox implementation', async () => {
-    const driver = new webdriver.Builder().forBrowser('firefox').build();
+  let driver;
+
+  beforeAll(() => {
+    driver = new webdriver.Builder().forBrowser('firefox').build();
     driver.setContext(Context.CHROME);
+  });
 
-    function runComparisonInFirefox(args) {
-      const { FileUtils } = Components.utils.import(
-        'resource://gre/modules/FileUtils.jsm'
-      );
-      const { NetUtil } = Components.utils.import(
-        'resource://gre/modules/NetUtil.jsm'
-      );
-
-      const allVersions = args[1];
-      const callback = args[args.length - 1];
-      const file = new FileUtils.File(args[0]);
-      const scriptUri = NetUtil.newURI(file);
-
-      Services.scriptloader.loadSubScript(scriptUri.spec, this);
-
-      const results = [];
-      for (const versions of allVersions) {
-        const [a, b] = versions;
-
-        let firefoxValue = Services.vc.compare(a, b);
-
-        // Normalize values to only have -1, 1 or 0.
-        if (firefoxValue < 0) {
-          firefoxValue = -1;
-        } else if (firefoxValue > 0) {
-          firefoxValue = 1;
-        }
-
-        results.push({
-          versions,
-          mozCompare: mozCompare(a, b),
-          firefox: firefoxValue,
-        });
-      }
-
-      callback(results);
-    }
-
-    const results = await driver.executeAsyncScript(
-      `(${runComparisonInFirefox})(arguments)`,
-      path.join(__dirname, '..', 'src', 'index.js'),
-      ALL_VERSIONS
-    );
-
-    expect.assertions(ALL_VERSIONS.length + 1);
-    // Make sure we actually have versions.
-    expect(ALL_VERSIONS).not.toHaveLength(0);
-
-    for (const result of results) {
-      // We pass `versions` so that it is displayed in the output in case of an
-      // error.
-      expect({
-        versions: result.versions,
-        value: result.mozCompare,
-      }).toEqual({
-        versions: result.versions,
-        value: result.firefox,
-      });
-    }
-
+  afterAll(() => {
     return driver.quit();
   });
+
+  it('should have versions to test', () => {
+    expect(ALL_VERSIONS).not.toHaveLength(0);
+  });
+
+  it.each(ALL_VERSIONS)(
+    'matches Firefox Services.vc.compare("%s", "%s")',
+    async (a, b) => {
+      let fxVcCompare = await driver.executeScript(
+        `return Services.vc.compare("${a}", "${b}");`
+      );
+
+      if (fxVcCompare < 0) {
+        fxVcCompare = -1;
+      } else if (fxVcCompare > 0) {
+        fxVcCompare = 1;
+      }
+
+      expect(mozCompare(a, b)).toEqual(fxVcCompare);
+    }
+  );
 });
