a3ae63cf12e69caf224fd1ac5ef94910d02aacf0
Mathieu Pillard <diox@users.noreply.github.com>
2024-03-28 12:48:23 +0100

    Add tests around version numbers resigning (#41)
    * Add tests around version numbers resigning
    
    * Add a few more edge cases
    
    * FIXME

--- tests/helpers.js
+++ tests/helpers.js
@@ -18,6 +18,9 @@ const VERSIONS_EQUALS = [
   ['100.0.00', '100.0.0.0'],
   ['567pre0', '567pre'],
   ['567pre0', '566+'],
+  // FIXME: should be equal (it is when using Firefox's Services.vc.compare())
+  // but mozCompare() currently returns 1.
+  // ['1.0resigned0', '1.0resigned'],
 ];
 
 const VERSIONS_LOWER_THAN = [
@@ -45,6 +48,8 @@ const VERSIONS_LOWER_THAN = [
   ['3.0.24', '24.0.3'],
   ['1.01.10', '1.001.100'],
   ['99.99.99.99', '100'],
+  ['1.0resigned', '1.0'],
+  ['1.0resigned1', '1.0'],
 ];
 
 const VERSIONS_GREATER_THAN = [
@@ -72,6 +77,20 @@ const VERSIONS_GREATER_THAN = [
   ['101', '100.99.99.99'],
   ['100.100', '100.99'],
   ['567pre1', '567pre0'],
+  ['1.1resigned1', '1.0'],
+  ['1.1resigned2', '1.1resigned1'],
+  ['1.1resigned10', '1.1resigned1'],
+  ['10resigned1', '9'],
+  ['10resigned2', '10resigned1'],
+  ['9.26.2resigned1', '9.26.1'],
+  ['9.26.2resigned2', '9.26.2resigned1'],
+  ['1.20240327.51resigned1bedbcf', '1.20240327.50master-2bedbcf'],
+  ['1.20240327.51resigned2bedbcf', '1.20240327.51resigned1bedbcf'],
+  ['1.20240327.51resigned2bedbcf', '1.20240327.50master-2bedbcf'],
+  ['1.1.1resigned1', '1.1.0beta1'],
+  ['1.1.1resigned2', '1.1.1resigned1'],
+  ['1.0resigned1', '1.0resigned0'],
+  ['1.0', '1.0resigned'],
 ];
 
 const ALL_VERSIONS = [
